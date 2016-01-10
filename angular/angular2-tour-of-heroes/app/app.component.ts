403000b4d5d40dd3e469de97cf1832e64be2c943
paulvivier <github@vivier.net>
2016-01-10 00:24:44 -0500

    Functioning List Items

--- angular/angular2-tour-of-heroes/app/app.component.ts
+++ angular/angular2-tour-of-heroes/app/app.component.ts
@@ -9,7 +9,16 @@ interface Hero {
     selector: 'my-app',
     template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
+<h2>My Heroes</h2>
+<ul class="heroes">
+  <li *ngFor="#hero of heroes">
+    <!-- each hero goes here -->
+ 	<span class="badge">{{hero.id}}</span> {{hero.name}}
+  </li>
+</ul> 
+
+
+   <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
     <div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name"></div>
     `
@@ -21,5 +30,23 @@ export class AppComponent {
     public hero: Hero = {
         id: 1, 
         name: 'Windstorm'
-};
+	};
+    public heroes = HEROES;
+
 }
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
+
+
+
