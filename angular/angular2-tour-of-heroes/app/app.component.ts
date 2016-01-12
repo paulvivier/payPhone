0e74480e029ccb0b2d03fd4a0a315b9b88d74ff8
paulvivier <github@vivier.net>
2016-01-12 00:05:57 -0500

    Completed Master/detail

--- angular/angular2-tour-of-heroes/app/app.component.ts
+++ angular/angular2-tour-of-heroes/app/app.component.ts
@@ -11,28 +11,81 @@ interface Hero {
     <h1>{{title}}</h1>
 <h2>My Heroes</h2>
 <ul class="heroes">
-  <li *ngFor="#hero of heroes">
+  <li *ngFor="#hero of heroes" 
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
     <!-- each hero goes here -->
  	<span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
 </ul> 
 
+<div *ngIf="selectedHero">
+   <h2>{{selectedHero.name}} details!</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
+    <div><label>name: </label><input [(ngModel)]="selectedHero.name" placeholder="name"></div>
+    </div>
+     `,
 
-   <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name"></div>
-    `
+styles:[`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 10em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0em;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #EEE;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0em 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0px 0px 4px;
+  }
+`]
+
 
 })
+
+
+
 export class AppComponent { 
 
     public title = 'Tour of Heroes';
-    public hero: Hero = {
-        id: 1, 
-        name: 'Windstorm'
-	};
+    public selectedHero: Hero;
     public heroes = HEROES;
-
+    onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
 var HEROES: Hero[] = [
