917a26387897bf419fcf4f5c66d43eac38fda6c6
Vivier <github@vivier.net>
2016-09-11 13:28:00 -0400

    Cleaned up comments. Moved number assignment to inside keypressed loop

--- arduino/keypad-single-analog/keypad-single-analog.ino
+++ arduino/keypad-single-analog/keypad-single-analog.ino
@@ -12,15 +12,15 @@ int keyPadValue = 0;   // value read from the keyboard
 int riseThreshold = 3;  // Rise on the slope for initial voltage ramp up when a keyPressed
 int keyLastPressed = -1;
 
-
 // Range for analog voltage for various keys. Adjust by modifying resistors. 
 char keyNames[] = {'1'  ,'2'  ,'3'  ,'4'  ,'5'  ,'6'  ,'7'  ,'8'  ,'9'  ,'0'  ,'*'  ,'#'};
 int keyLows[]  = {730  ,680  ,200  ,570  ,456  ,180  ,390  ,340  ,156  ,119  ,126  ,83};
 int keyHighs[] = {890  ,702  ,213  ,588  ,485  ,189  ,402  ,349  ,163  ,125  ,131  ,89};
 
+// Set up buffer to hold the number to be called and an index to track length of the array
+// (I'm sure there's a better way, but I don't know it yet).
 char numberToCall[11];
 int numberIndex = 0;
-
 
 void setup(){
   Serial.begin(9600);  //hardware serial to PC  
@@ -40,84 +40,48 @@ void loop(){
       if (keyLastPressed + riseThreshold > keyPadValue){break;}  
 //      Serial.println("Still Rising");
    }
-
-   while (keyPadValue < 75) {
-   keyLastPressed = -1; 
-   delay (30);
-//   Serial.println("Reset lastKeyboardValue");
-   keyPadValue = analogRead(keyboardPin); // read the keyboard value (0 - 1023)
-
+   while (keyPadValue < 75) { // reset values once keypad is stop being pressed
+     keyLastPressed = -1; 
+     delay (30);
+     keyPadValue = analogRead(keyboardPin); // read the keyboard value (0 - 1023)
    }
 
 // Once the number has stablized, see what the reading is. 
     for (int i = 0; i < 12; i++){
-     
-    // Take your reading now, sir
+      
+    // Take your reading now
     // Find the key that was pressed by matching the range.  
         while ((keyPadValue > keyLows[i]) && (keyPadValue < keyHighs[i])){
-              numberToCall[numberIndex] = keyNames[i]; // determine what keyName in the mapping matches your volt range
-
-    
-              // Since we're making rapid loops and continuing to read the voltage
-              // this checks to see if you still have the same key pressed down that you
-              // had presssed a few hundred miliseconds ago. 
+              // Is this the same key that was pressed a few miliseconds? 
               if (keyLastPressed != keyPressed){
-
-                // if it's not the same key we've seen before then this is the
-                // number to use
-//                numberToCall[numberIndex] = keyPressed; // take key pressed and put it into the number to call buffer
-//                Serial.print("keyPressed:");
-//                Serial.println(keyPressed);
-
-//                Serial.print("numberToCall:");
-                Serial.print(numberToCall[numberIndex]);
-//                Serial.print("numberIndex: ");
-//                Serial.println(numberIndex);
-                               
-                ++numberIndex;
-         
-         //       Serial.print("Dialing:");
-
-         //       Serial.println(numberToCall);
-         //       Serial.print("Length of numberToCall: ");
-         //       Serial.println(numberToCall.length());
+                // Store key pressed into next available slot for number to call
+                numberToCall[numberIndex] = keyNames[i];
+                Serial.print(numberToCall[numberIndex]);                               
+                ++numberIndex; 
               }
-            
-
+              
               // Keeps keyPressed from getting resampled while button is still pressed.
                 while (keyPadValue > 75){
-               //   Serial.print("                     key:");
-               //   Serial.println(keyNames[i]);
                   keyPadValue = analogRead(keyboardPin); // read the keyboard value (0 - 1023)
                 }
 
-              // 
-              keyLastPressed = keyPressed;
+              keyLastPressed = keyPressed; // before leaving, hold on to the last valid keyPressed
               
-        
         } // while loop
-
-    
     } // int i loop
 
 
+    // Once we have 10 digits in the array, we have a valid phone number.
+    // Array index starts at 0.
     if (numberToCall[9]) {
      Serial.println();
      Serial.print("Calling: ");
      Serial.println(numberToCall);
-//    numberToCall[0] = (char)0;
-//     numberIndex = (char)0;  
-
-//     Serial.println("Clearing numberToCall Array: ");
-     // defect - numberToCall isn't getting 'zeroed'. 
+      // Clearing arrays 
       for( int i = 0; i < sizeof(numberToCall);  ++i ){
-//         Serial.print("int i: ");
-//         Serial.println(i);
          numberToCall[i] = (char)0;  
          numberIndex = (char)0;  
-//         Serial.println(numberToCall[i]);
         } 
-  
       }
           
     keyPadValue = analogRead(keyboardPin); // See if it's time to end the loop     
